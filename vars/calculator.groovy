149c15e88451dc3bf4efd33fd64448df0d032dc4
anjikdk <45225583+anjikdk@users.noreply.github.com>
2021-05-30 19:10:17 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -2,3 +2,8 @@ def add(i, j)
 {
   echo "Sum of ${i} and ${j} value is ${i+j}"
 }
+
+def simpleInterest(p, t, r)
+{
+  echo "Simple interest for ${p} and time ${t} with rate if interest is ${r} : (p*t*r)/100"
+}
